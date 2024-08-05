fdb09b7703a729f1524a08e5924e92e563101f29
El_Actor <20233tn075@utez.edu.mx>
2024-08-04 21:41:43 -0600

    xd

--- src/main/java/mx/edu/utez/integradiratjuans/controller/InsertarPreguntaServlet.java
+++ src/main/java/mx/edu/utez/integradiratjuans/controller/InsertarPreguntaServlet.java
@@ -43,19 +43,42 @@ public class InsertarPreguntaServlet extends HttpServlet {
             pregunta.setRespuesta(respuesta);
             pregunta.setIdExamen(idExamen);
 
-            // Añadir las opciones si las hay
-            int opcionIndex = 1;
-            while (request.getParameter("questions[" + index + "].option" + opcionIndex) != null) {
-                String opcionTexto = request.getParameter("questions[" + index + "].option" + opcionIndex);
-                boolean esCorrecta = "true".equals(request.getParameter("questions[" + index + "].correctOption" + opcionIndex));
+            // Manejo de opciones para preguntas de opción múltiple y varias respuestas
+            if ("opcion_multiple".equals(tipo) || "varias_respuestas".equals(tipo)) {
+                int opcionIndex = 1;
+                while (request.getParameter("questions[" + index + "].option" + opcionIndex) != null) {
+                    String opcionTexto = request.getParameter("questions[" + index + "].option" + opcionIndex);
+                    boolean esCorrecta = "true".equals(request.getParameter("questions[" + index + "].correctOption" + opcionIndex));
 
-                Opcion opcion = new Opcion();
-                opcion.setOpcion(opcionTexto);
-                opcion.setCorrecta(esCorrecta);
+                    Opcion opcion = new Opcion();
+                    opcion.setOpcion(opcionTexto);
+                    opcion.setCorrecta(esCorrecta);
 
-                pregunta.addOpcion(opcion);
-                opcionIndex++;
+                    pregunta.addOpcion(opcion);
+                    opcionIndex++;
+                }
             }
+
+            // Manejo de opciones para preguntas de verdadero o falso
+            if ("verdadero_falso".equals(tipo)) {
+                Opcion opcionVerdadero = new Opcion();
+                opcionVerdadero.setOpcion("Verdadero");
+                opcionVerdadero.setCorrecta("true".equals(request.getParameter("questions[" + index + "].correctOption1")));
+
+                Opcion opcionFalso = new Opcion();
+                opcionFalso.setOpcion("Falso");
+                opcionFalso.setCorrecta("true".equals(request.getParameter("questions[" + index + "].correctOption2")));
+
+                pregunta.addOpcion(opcionVerdadero);
+                pregunta.addOpcion(opcionFalso);
+                System.out.println("correctOption1: " + request.getParameter("questions[" + index + "].correctOption1"));
+                System.out.println("correctOption2: " + request.getParameter("questions[" + index + "].correctOption2"));
+                System.out.println("opcionVerdadero.isCorrecta(): " + opcionVerdadero.isCorrecta());
+                System.out.println("opcionFalso.isCorrecta(): " + opcionFalso.isCorrecta());
+
+            }
+
+
 
             preguntas.add(pregunta);
             index++;
